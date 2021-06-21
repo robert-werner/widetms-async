0b1025bff2bef8a9388f440c09b497a0622f5ae4
Leonid Kolesnichenko <xperience439@gmail.com>
2021-06-21 12:36:00 +0300

    -- (WIP) Add container app

--- config/celery_app.py
+++ config/celery_app.py
@@ -1,10 +1,13 @@
 from celery import Celery
 from config.environment import CELERY_RESULT_URL, CELERY_BROKER_URL
 
-app = Celery('widetms.producer', broker=CELERY_BROKER_URL, backend=CELERY_RESULT_URL)
+app = Celery('widetms.producer', broker=CELERY_BROKER_URL, backend=r"redis://172.17.0.1:6379")
 
 app.conf.update(
     task_serializer='pickle',
     result_serializer='pickle',
     accept_content=['pickle']
 )
+
+app.conf.task_routes = {'widetms.worker.tile': {'queue': 'tiler'},
+                        'widetms.builder.build': {'queue': 'builder'}}
--- routers/tms.py
+++ routers/tms.py
@@ -1,19 +1,24 @@
 from config.celery_app import app as celery_app
 from fastapi import APIRouter, Query, HTTPException
-
+from fastapi.responses import StreamingResponse
 from routers.tms_logic.router_logic import form_tasks
+import time
 
 router = APIRouter()
 
 
 @router.get('/tms/{alias}/{z}/{x}/{y}.tif@{res}')
 async def tms(alias: str, z: int, x: int, y: int, res: int, channels: str = Query(None, regex=r"\(([^()]+)\)")):
+    start_time = time.time()
     try:
         bands = []
         tasks = await form_tasks(alias, channels, z, x, y, res)
         for task in tasks:
             bands.append(celery_app.send_task('widetms.worker.tile', (task,)))
-        bands = [band.get() for band in bands]
-        return len(bands)
+        tile = celery_app.send_task('widetms.builder.build', (bands, ))
+        tile = tile.get()
+        return StreamingResponse(open(tile, mode='rb'), media_type='image/tiff', headers={
+            
+        })
     except Exception as e:
-        raise HTTPException(status_code=400, detail={'type': str(e.__class__), 'text': str(e)})
+        raise HTTPException(status_code=400, detail={'type': str(e.__class__), 'text': str(e), 'execution_time': f"{time.time() - start_time} seconds"})
